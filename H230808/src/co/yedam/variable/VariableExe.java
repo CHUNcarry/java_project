82a5e065381a1ce858bd170a20fec38c98f8c486
admin <admin@YD5-13>
2023-08-08 12:19:00 +0900

    5th

---
+++ H230808/src/co/yedam/variable/VariableExe.java
@@ -0,0 +1,21 @@
+package co.yedam.variable;
+
+public class VariableExe {
+	public static void main(String[] args) {
+		//변수선언 
+		
+		//정수형 변수 byte short long 		//byte : 128-127
+		//short
+		
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		int sum1 = num1 + num2;
+		
+		System.out.println(sum1);
+		
+
+	}
+
+}
