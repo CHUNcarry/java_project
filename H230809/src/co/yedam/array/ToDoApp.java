a26acde5372ab879c6fc87f0ae61063372c8837b
admin <admin@YD5-13>
2023-08-10 17:44:36 +0900

    0810 mid

--- H230809/src/co/yedam/array/ToDoApp.java
+++ H230809/src/co/yedam/array/ToDoApp.java
@@ -2,7 +2,17 @@ package co.yedam.array;
 
 public class ToDoApp {
 	public static void main(String[]args) {}
-		//등록/완료/조회/미완료
-	//1 할일 0810
-	}
+	//등록/완료/조회/미완료
+	//등록 : 1 할일 0810
+	//완료 : 등록>>완료 클래스이동
+	//조회 : 날짜 입력 > 등록 완료 출력
+	//if 날짜
+	
+	//미완료 : 완료 클래스에 없는 요소 boolean false
+	
+	//보여주는 기준 순번
+	
+
+
+}
 
